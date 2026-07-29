5e07d1568009b11b674c06296fe20caad1de57d6
Manish Singh <manishsingh0434@gmail.com>
2026-07-29 17:32:51 +0530

    Initial commit

---
+++ android/app/src/main/java/com/manish/ritual/MainActivity.java
@@ -0,0 +1,5 @@
+package com.manish.ritual;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.manish.ritual',
+  appName: 'Ritual',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
